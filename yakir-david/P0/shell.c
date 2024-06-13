cbabcb46b2e6df7fcac1fde87aea636ddac7c401
yakir-david <yakir-david@spring2024-iit.kdlp.underground.software>
2024-06-13 16:12:19 +0300

    lvl1: Reading lines + exiting on EOF
    The shell reads lines of user input, but doesn't need to do anything with them.
    It just prints a new prompt before each line.
    
    The shell loops until it gets EOF from user input (ctrl+d).
    
    The shell exits with code 0.
    
    Signed-off-by: yakir-david <yakir-david@spring2024-iit.kdlp.underground.software>

--- yakir-david/P0/shell.c
+++ yakir-david/P0/shell.c
@@ -4,11 +4,31 @@
 
 int main()
 {
+	printEntry();
 	PromptPtr p_ptr = Palloc();
 	if (p_ptr == NULL)
 		exit(EXIT_FAILURE);
 
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread = 0;
+
 	printPrompt(p_ptr);
+	while ((nread = getline(&line,&len,stdin)) != -1)
+	{
+		if (strcmp(line,"^D") == 0)
+			break;
+
+		//puts("");
+		printPrompt(p_ptr);
+	}
+
+	goto FREE_ALL;
+	FREE_ALL:
+		if (line != NULL)
+			free(line);
+		if (p_ptr != NULL)
+			Pfree(p_ptr);
 	puts("");
 	return 0;
 }
